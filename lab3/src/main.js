f317c14be031bda5ebff3643df8d0b009f052925
Illia <simonewarlet@gmail.com>
2024-09-18 13:07:01 +0300

    Change gauss_js and add tests

--- lab3/src/main.js
+++ lab3/src/main.js
@@ -6,5 +6,9 @@ const n = 3;
 const mtrx = read_input();
 
 
-console.log(gauss(mtrx))
-mtrx.printm();
+//console.log(gauss(mtrx))
+//mtrx.printm();
+
+let mtrx2 = new Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+mtrx2.printm()
+console.log(mtrx2)
---
+++ lab3/test/test.js
@@ -0,0 +1,75 @@
+import { expect } from 'chai';
+import matrix from '../gauss_js/matrix.js';
+import sinon from "sinon";
+
+describe('Matrix Class with sinon.mock Tests', function () {
+    let matrixInstance;
+    let mock;
+
+    beforeEach(function () {
+        matrixInstance = new matrix(3);
+        mock = sinon.mock(matrixInstance);
+    });
+
+    afterEach(function () {
+        mock.restore();
+    });
+
+    it('should call get_rows and get_cols when calling exists_zero_row', function () {
+        mock.expects("get_rows").atLeast(1);
+        //mock.expects("get_cols").atLeast(1);
+        mock.expects("get_cols").never();
+    
+        matrixInstance.set(0, 0, 0);
+        matrixInstance.set(0, 1, 0);
+        matrixInstance.set(0, 2, 0);
+    
+        matrixInstance.exists_zero_row();
+    
+        mock.verify();
+    });
+    
+
+    it('should call get_rows and get_cols when calling exists_zero_row', function () {
+        mock.expects("get_rows").once().returns(3);
+        mock.expects("get_cols").once().returns(4);
+
+        matrixInstance.set(0, 0, 0);
+        matrixInstance.set(0, 1, 0);
+        matrixInstance.set(0, 2, 0);
+        matrixInstance.set(0, 3, 0);
+
+        const result = matrixInstance.exists_zero_row();
+        expect(result).to.be.true;
+
+        mock.verify();
+    });
+
+    it('should call get_cols and manipulate rows in mull_add', function () {
+        mock.expects("get_cols").once().returns(4);
+        mock.expects("get_rows").never();
+
+        matrixInstance.set(0, 0, 1);
+        matrixInstance.set(1, 0, 2);
+
+        matrixInstance.mull_add(0, 1, 2);
+
+        expect(matrixInstance.get(0, 0)).to.equal(5);
+
+        mock.verify();
+    });
+
+    it('should swap rows when calling swap_with_nonzero_row', function () {
+        mock.expects("get_rows").once().returns(3);
+        mock.expects("get_cols").twice().returns(4);
+
+        matrixInstance.set(0, 0, 0);
+        matrixInstance.set(1, 0, 1);
+
+        matrixInstance.swap_with_nonzero_row(0);
+
+        expect(matrixInstance.get(0, 0)).to.equal(1);
+
+        mock.verify();
+    });
+});
